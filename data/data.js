e353be2ef9b7d416bea8ec1e64c3ef606dd751e3
alexandby <alexanderbyshnyov@gmail.com>
2021-11-07 16:18:02 +0300

    added games data, img folder

---
+++ data/data.js
@@ -0,0 +1,14 @@
+export const GAMES_DATA = [
+  {
+    id: 'game1',
+    name: 'CS:GO',
+    details: 'Details 2',
+    logo: '/img/csgo.jpg',
+  },
+  {
+    id: 'game2',
+    name: 'DOTA 2',
+    details: 'Details 1',
+    logo: '/img/1333996387__dota-2.jpg',
+  },
+];
